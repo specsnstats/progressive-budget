a8e91831d2c93d64029ec23ab6fef916a05efc78
SpecsNStats <specsnstats@gmail.com>
2021-11-18 18:34:51 -0700

    changed around the order of caches in service worker?

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,11 +2,11 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
-  "db.js",
   "/index.html",
+  "/style.css",
+  "db.js",
   "/index.js",
   "/manifest.webmanifest",
-  "/style.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png"
 ];
